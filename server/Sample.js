c7957b3b91d326343f2ffce95e92f3c9ae8b7787
Harish Swaminathan <swaminathan.harish@gmail.com>
2016-02-21 15:44:05 +0530

    Removing files from wrong directory

--- server/Sample.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Sample = new Schema({ sampleText: String}, { collection: 'SampleTable' })
-
-module.exports = mongoose.model('Sample', Sample);
--- server/SampleController.js
+++
@@ -1,9 +0,0 @@
-var Sample  = require('../model/Sample.js');
-
-module.exports.getSample=function(req,res){
-	Sample.find({})
-              .exec(
-                function(err,results){
-            	    res.json(results);
-                });
-}
